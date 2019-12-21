f45722dc62475e09eba4a2a125f3745d8e6e441d
alisa <balakirska@ucu.edu.ua>
2019-12-21 19:41:29 +0200

    Final commit

--- src/main/java/ua/edu/ucu/iterator/FlatMapIterator.java
+++ src/main/java/ua/edu/ucu/iterator/FlatMapIterator.java
@@ -9,25 +9,25 @@ public class FlatMapIterator implements Iterator<Integer> {
 
     private Iterator<Integer> oldIter;
     private IntToIntStreamFunction operator;
-    private StreamIterator tempIter;
+    private StreamIterator streamIter;
     private boolean check;
 
     public FlatMapIterator(Iterator<Integer> previous, IntToIntStreamFunction operator) {
         this.oldIter = previous;
         this.operator = operator;
         this.check = false;
-        this.tempIter = new StreamIterator();
+        this.streamIter = new StreamIterator();
     }
 
     @Override
     public boolean hasNext() {
-        return this.tempIter.hasNext() || this.oldIter.hasNext() || !this.check;
+        return this.streamIter.hasNext() || this.oldIter.hasNext() || !this.check;
     }
 
     @Override
     public Integer next() {
-        if (this.tempIter.hasNext()) {
-            return this.tempIter.next();
+        if (this.streamIter.hasNext()) {
+            return this.streamIter.next();
 
         } else {
             if (this.oldIter.hasNext()) {
@@ -37,8 +37,8 @@ public class FlatMapIterator implements Iterator<Integer> {
                 for (int el : array) {
                     list.add(el);
                 }
-                this.tempIter = new StreamIterator(list);
-                Integer next = this.tempIter.next();
+                this.streamIter = new StreamIterator(list);
+                Integer next = this.streamIter.next();
 
                 if (!this.oldIter.hasNext()) {
                     this.check = true;
